0652945382a78a375624aa00c21f3e1ae646f184
Zack Feldstein <zack.feldstein@blackboard.com>
2018-09-27 23:57:25 -0500

    initial commit

---
+++ docs/docs.py
@@ -0,0 +1,82 @@
+import slate
+import spacy
+
+from pymongo import MongoClient
+
+
+client = MongoClient(
+    'localhost',
+    username='root',
+    password='example',
+    authMechanism='SCRAM-SHA-256'
+)
+nlp = spacy.load('en_core_web_sm')
+
+
+def fact_clean(facts, subject):
+    # Clean Facts remove subject and det's
+    facts = ' '.join(facts)
+    facts = nlp(facts)
+    final_facts = []
+    for i in facts:
+        if i.pos_ in ['DET', 'PUNCT'] or i.text == subject.text:
+            pass
+        else:
+            final_facts.append({"object": i.text})
+    return final_facts
+
+
+def clean_subject(subject):
+    invalid_subjects = ['ADP']
+    if subject.pos_ in invalid_subjects:
+        return None
+    else:
+        return subject
+
+
+# TODO(zf) Change to filename as first arg
+def parse_facts(doc):
+    doc = nlp(doc)
+    knowledge = {}
+    for sent in doc.sents:
+        root = [token for token in sent if token.head == token][0]
+        if root:
+            if len(list(root.lefts)) < 1:
+                continue
+            subject = list(root.lefts)[0]
+        else:
+            continue
+        if not clean_subject(subject):
+            continue
+        # Gather Facts
+        facts = []
+        for chunk in sent.noun_chunks:
+            facts.append(chunk.text)
+        clean_facts = fact_clean(facts, subject)
+        knowledge[subject] = clean_facts
+    return knowledge
+
+
+def clean_doc(doc):
+    pass
+
+
+def create_doc_col(db, doc_name, doc, file_type='txt'):
+    # PDFSyntaxError
+    if file_type == 'pdf':
+        with open(doc, 'rb') as f:
+            doc = slate.PDF(f)
+            doc = str(doc)
+    else:
+        doc = open(doc).read()
+    db = client[db]
+    db_data = parse_facts(doc)
+    for subject, facts in db_data.items():
+        subject_str = str(subject)
+        document = {
+            'subject': subject_str,
+            'facts': db_data[subject]
+        }
+        db[doc_name].insert_one(document)
+    # TODO(zf) add real check and return real value
+    return "Sucesfully created doc database"
---
+++ main.py
@@ -0,0 +1,100 @@
+#!flask/bin/python
+import json
+import os
+
+from docs import docs
+from flask import Flask
+from flask import jsonify
+from flask import request
+from pymongo import MongoClient
+
+app = Flask('hunary_api')
+app.config.from_pyfile('conf/hunary.conf')
+
+client = MongoClient(app.config['DB_HOST'],
+                     username=app.config['DB_USER'],
+                     password=app.config['DB_PASSWD'],
+                     authMechanism='SCRAM-SHA-256')
+
+
+def jwrap(key, values):
+    json_obj = jsonify({key: values})
+    return json_obj
+
+
+# Log's a message
+def log(var, msg='Here is your var'):
+    app.logger.info("{} {}".format(msg, var))
+
+
+# exampl/api/v1/mars/subject
+@app.route('/api/v1/<acct>/docs/<doc_name>/search', methods=['POST'])
+def search_doc(acct, doc_name):
+    content = request.get_json()
+    if content:
+        # Must specify a subject to query
+        if content.get('subject') is None and content.get('facts') is None:
+            return jwrap('error', 'specify subject or facts in request')
+        db = client[acct]
+        collection = db[doc_name]
+        result = []
+        if content.get('facts') is not None:
+            if content.get('subject') is None:
+                results = collection.find({'facts.object': content['facts']})
+            else:
+                results = collection.find(
+                    {
+                        'subject': content['subject'],
+                        'facts.object': content['facts']
+                    }
+                )
+        else:
+            results = collection.find({'subject': content['subject']})
+        for row in results:
+            for fact in row['facts']:
+                result.append({"object": fact['object']})
+        return jwrap('facts', result)
+    else:
+        app.logger.warn("No json found %s" % content)
+        return "NoJsonError"
+
+
+@app.route('/api/v1/<acct>/docs', methods=['GET'])
+def list_docs(acct):
+    db = client[acct]
+    collections = db.list_collection_names()
+    return jwrap('docs', collections)
+
+
+@app.route('/api/v1/<acct>/docs/<doc_name>', methods=['DELETE'])
+def delete_doc(acct, doc_name):
+    db = client[acct]
+    collections = db.drop_collection(doc_name)
+    if collections['ok'] == 0.0:
+        return jwrap(
+            'docs', 'doc {} not found or doesn\'t exist'.format(doc_name)
+            )
+    else:
+        return jwrap('docs', '{} deleted sucessfully'.format(doc_name))
+
+
+# DOC parse and upload
+@app.route('/api/v1/<acct>/docs/<doc_name>', methods=['POST'])
+def upload_doc(acct, doc_name):
+    # checking if the file is present or not.
+    if 'file' not in request.files:
+        # TODO(zf) add a 400 return code
+        return "No File"
+    doc = request.files['file']
+    file_type = json.load(request.files['file_type'])
+    doc_path = os.path.abspath('./uploads/docs/{}/{}'.format(acct, doc_name))
+    open(doc_path, 'w')
+    doc.save(doc_path)
+    created_doc = docs.create_doc_col(acct, doc_name, doc_path, file_type)
+    os.remove(doc_path)
+    return jwrap("docs", created_doc)
+    return "Doc {} successfully saved".format(doc_path)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
